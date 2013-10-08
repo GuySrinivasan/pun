5386b3e47680f0ea13239ca095d4582763c69a15
Guy Srinivasan <srinivgp@gmail.com>
2013-10-08 13:55:27 -0700

    larger wordlists. very slow.

--- importdict.py
+++ importdict.py
@@ -1,6 +1,13 @@
 #import numpy
 
-wordlists = [ "american-words.35", "english-words.35" ]
+wordlists35 = [ "american-words.35", "english-words.35" ]
+wordlists = [
+"american-words.10", "american-words.55", "english-words.10", "english-words.55",
+"american-words.20", "american-words.60", "english-words.20", "english-words.60",
+"american-words.35", "english-words.35",
+"american-words.40", "english-words.40",
+"american-words.50", "english-words.50"
+]
 
 def read_wordlists(wordlists):
 	master = []
@@ -16,11 +23,9 @@ def levenshtein_distance(typed, word):
 	# of word
 	T = len(typed)
 	W = len(word)
-	#dist = numpy.zeros((T+1)*(W+1)).reshape(T+1, W+1)
 	dist = [[0 for x in range(W+1)] for x in range(T+1)]
 	for i in range(T+1):
 		for j in range(W+1):
-			#print(i,j)
 			if min(i,j) == 0:
 				dist[i][j] = max(i,j)
 			else:
@@ -28,7 +33,6 @@ def levenshtein_distance(typed, word):
 				insertion = dist[i][j-1] + 1
 				match = dist[i-1][j-1] + (0 if typed[i-1] == word[j-1] else 1)
 				dist[i][j] = min(deletion, insertion, match)
-			#print(i,j,dist[i][j])
 	return dist[T][W]
 
 def smallest_distance(typed, dict):
@@ -47,7 +51,7 @@ def smallest_distance(typed, dict):
 debug = False
 
 if debug:
-	temp = read_wordlists(wordlists)
+	temp = read_wordlists(wordlists35)
 
 	for i in range(0,len(temp),(int)(len(temp)/100)):
 		print(temp[i])
